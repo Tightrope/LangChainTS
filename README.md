6b703e8fc0d684f6c6e667befa8e5c549c0c3aaa
Tightrope <7082808+Tightrope@users.noreply.github.com>
2023-09-09 11:52:22 -0400

    Lots of experiments and changes

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Developer Notes
 
+## Try
+* [Return Source Documents](https://js.langchain.com/docs/modules/chains/popular/vector_db_qa#return-source-documents)
+* [VectorStore QA](https://js.langchain.com/docs/modules/chains/popular/vector_db_qa)
+* [LangChain TS Starter](https://github.com/domeccleston/langchain-ts-starter/tree/main)
+* [OpenAI Node](https://github.com/openai/openai-node)
+  * Try one of the examples
+  * Use openai 4.0.0 — latest gives `_shims` error
+
+
 ## Environment Variables
 The following environment variables are required to run the code:
    * `OPENAI_API_KEY` - OpenAI API Key
@@ -18,6 +27,9 @@ Place the above in a `.env` file in the root of the project.
 * OpenAI `text-embedding-ada-002` outputs [1536 dimensions](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings)
 
 ## References
+* [LangChain Install](https://js.langchain.com/docs/get_started/installation)
+  * [Examples](https://github.com/hwchase17/langchainjs/tree/main/examples)
+* [LangChain Python QA Tutorial](https://python.langchain.com/docs/use_cases/question_answering/how_to/vector_db_qa)
 * [Langchain Tutorial For Typescript and Javascript Developers](https://github.com/mayooear/langchain-js-tutorial)
 * [LangChain Tutorials](https://python.langchain.com/en/latest/getting_started/tutorials.html)
 * [LangChain Docs](https://js.langchain.com/docs/)
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,93 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import {OpenAIEmbeddings} from "langchain/embeddings/openai";
-import {PineconeStore} from "langchain/vectorstores/pinecone";
-import {PineconeClient} from "@pinecone-database/pinecone";
-import {RetrievalQAChain} from "langchain/chains";
 import {OpenAI} from "langchain/llms/openai";
+import {OpenAIEmbeddings} from "langchain/embeddings/openai";
+import * as fs from "fs";
+import {HNSWLib} from "langchain/vectorstores/hnswlib";
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {RetrievalQAChain} from "langchain/chains";
+import {PineconeStore} from "langchain/vectorstores/pinecone";
+import {PineconeClient} from "@pinecone-database/pinecone"
 
-const OPENAI_MODEL = "text-embedding-ada-002";
+const index_name = "test";
 
 async function main(){
-    const openAIEmbeddings = new OpenAIEmbeddings({openAIApiKey: process.env.OPENAI_API_KEY, modelName: OPENAI_MODEL});
+    const contentFiles = ['./data/elon_5_rules.txt'];
 
-    // Add text files to Pinecone
     const pineconeClient = new PineconeClient();
     await pineconeClient.init({
         apiKey: process.env.PINECONE_API_KEY,
         environment: process.env.PINECONE_ENVIRONMENT
     });
 
-    const contentFiles = ['../data/beato_SFisOTDzGuE.txt', '../data/beato_xKIC9zbSJoE.txt', '../data/beato_ZavJLr5Otq4.txt'];
-    let pineconeStore = await PineconeStore.fromTexts(contentFiles, {}, openAIEmbeddings, {
-        pineconeIndex: pineconeClient.Index("beato") });
+    const openAIEmbeddings = new OpenAIEmbeddings({openAIApiKey: process.env.OPENAI_API_KEY});
 
-    // Create a RetrievalQAChain
-    const model = new OpenAI({openAIApiKey: process.env.OPENAI_API_KEY, modelName: OPENAI_MODEL});
-    const qaChain = RetrievalQAChain.fromLLM(model, pineconeStore.asRetriever());
+    // Create an index named if it does not exist after listing the indices
+    const indices = await pineconeClient.listIndexes()
+    console.log(`Indices: ${JSON.stringify(indices)}`);
+
+    if(!indices.includes(index_name)){
+        await pineconeClient.createIndex( {
+            createRequest: {
+                name: index_name,
+                dimension: 1536
+            }
+        });
+        const pineconeIndex = pineconeClient.Index(index_name);
+
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 0
+        });
+
+        for(const contentFile of contentFiles){
+            const content = fs.readFileSync(contentFile, {encoding: "utf-8"});
+            const chunkedDocuments = await textSplitter.createDocuments([content], [{url: contentFile}]);
+            console.log(`Index document ${contentFile} into Pinecone index ${index_name}`);
+            await PineconeStore.fromDocuments(chunkedDocuments, openAIEmbeddings, { pineconeIndex: pineconeIndex });
+        }
+    }
+
+    // const vectorStore = await initHNSWLibVectorStore(contentFiles);
+    const vectorStore = await PineconeStore.fromExistingIndex(openAIEmbeddings, {
+        pineconeIndex: pineconeClient.Index(index_name) });
+
+    const model = new OpenAI({openAIApiKey: process.env.OPENAI_API_KEY});
+    const qaChain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
+        returnSourceDocuments: true
+    });
+    const query = "What is Elon's third step for success?";
     const answer = await qaChain.call({
-        query: "Describe a unique chord change",
+        query: query
     });
 
-    console.info(`The answer is: ${answer}`);
+    console.info(`The answer is: ${JSON.stringify(answer)}`);
+}
+
+async function initHNSWLibVectorStore( contentFiles: string[]){
+    const openAIEmbeddings = new OpenAIEmbeddings({openAIApiKey: process.env.OPENAI_API_KEY});
+
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 0
+    });
+
+    const vectorStore = new HNSWLib(openAIEmbeddings, {
+        space: "cosine",
+        numDimensions: 1536
+    });
+
+    for(const contentFile of contentFiles){
+        const content = fs.readFileSync(contentFile, {encoding: "utf-8"});
+        const chunkedDocuments= await textSplitter.createDocuments([content], [{url: contentFile}]);
+        console.log(`Index document ${contentFile} into HNSWLib Vector Store`);
+        await vectorStore.addDocuments(chunkedDocuments);
+    }
+
+    return vectorStore;
 }
 
 main().then(() => {
     console.log("done");
-});``
+});
